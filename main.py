815c906693162687e5ba4be1fe00f650b0292d27
Pukabyte <pukabyte@gmail.com>
2025-03-02 19:19:43 +1300

    Docker Image creation

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import random
 import string
 
 # Application version - update this when creating new releases
-VERSION = "0.0.5"
+VERSION = "0.0.8"
 
 # Create a logger for this module
 logger = logging.getLogger(__name__)
